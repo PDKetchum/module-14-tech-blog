f9dbfa3197957749328ef3e75737a858617fce32
PDKetchum <pxiong265@gmail.com>
2022-10-25 19:43:05 -0500

    fixed seed.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,8 +5,25 @@ const withAuth = require("../utils/auth");
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
-  const locationData = await Post.findAll({ raw: true });
-  res.render("homepage");
+  try {
+    const dbPostData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+
+    const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+    res.render("homepage", {
+      posts,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
